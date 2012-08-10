6cde81f4dbbadf8f71f2d7520980620c3e3631a8
Keith Bennett <keithrbennett@gmail.com>
2012-08-10 17:18:40 -0400

    Now hold on only to @table_model, and pass it into the action.

--- lib/view/main_frame.rb
+++ lib/view/main_frame.rb
@@ -11,20 +11,17 @@ java_import 'java.awt.BorderLayout'
 
 class MainFrame < JFrame
 
-  attr_accessor :table
+  attr_accessor :table_model
 
   def initialize(life_model)
     super('The Game of Life')
     set_default_close_operation(JFrame::EXIT_ON_CLOSE)
-    @table = JTable.new(LifeTableModel.new_instance(life_model))
+    @table_model = LifeTableModel.new_instance(life_model)
+    table = JTable.new(@table_model)
     add(header, BorderLayout::NORTH)
-    add(@table, BorderLayout::CENTER)
-    add(JButton.new(ShowNextGenerationAction.new(@table)), BorderLayout::SOUTH)
+    add(table, BorderLayout::CENTER)
+    add(JButton.new(ShowNextGenerationAction.new(@table_model)), BorderLayout::SOUTH)
     pack
-  end
-
-  def life_table_model
-    @table.get_model
   end
 
   def header
@@ -32,24 +29,24 @@ class MainFrame < JFrame
     label = JLabel.new(header_text)
     inner_panel.add(label)
     panel = JPanel.new(BorderLayout.new)
-    panel.add(inner_panel)
+    panel.add(inner_panel, BorderLayout::CENTER)
     panel
   end
 
   def header_text
-    "<html><center><h2>The Game of Life (#{life_table_model.getRowCount} x #{life_table_model.getColumnCount})</h2></center></html"
+    "<html><center><h2>The Game of Life (#{@table_model.getRowCount} x #{@table_model.getColumnCount})</h2></center></html"
   end
 
   class ShowNextGenerationAction < AbstractAction
 
-    def initialize(table)
+    def initialize(tableModel)
       super("Show Next Generation")
-      @table = table
+      @table_model = tableModel;
     end
 
     def show_next_generation
-      @table.getModel.go_to_next_generation
-      @table.getModel.fire_table_data_changed
+      @table_model.go_to_next_generation
+      @table_model.fire_table_data_changed
     end
 
     def actionPerformed(event)
