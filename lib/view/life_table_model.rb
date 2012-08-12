1a0d48f70941bdad6a8f4e259f9376c17bc4cc63
Keith Bennett <keithrbennett@gmail.com>
2012-08-12 12:31:52 -0400

    Broken now; beginning of extracting Generations class from table model class.

--- lib/view/life_table_model.rb
+++ lib/view/life_table_model.rb
@@ -3,7 +3,6 @@ java_import javax.swing.table.AbstractTableModel
 java_import javax.swing.JOptionPane
 
 
-
 class LifeTableModel < AbstractTableModel
 
   attr_accessor :life_model
@@ -47,34 +46,20 @@ class LifeTableModel < AbstractTableModel
     nil
   end
 
-  def current_generation_num=(new_num)
-    @current_generation_num = new_num
-    @current_num_change_handlers.each { |handler| handler.call(@current_generation_num) }
-  end
-
   def go_to_next_generation
+    ensure_next_generation_loaded
     next_generation_num = current_generation_num + 1
-    next_generation_is_cached = next_generation_num < generations.size
+    next_generation = generations[next_generation_num]
 
-    if next_generation_is_cached
-      next_generation = generations[next_generation_num]
+    # Last generation is defined as the generation after which
+    # there are no changes.
+    if next_generation == life_model
+      @last_generation_num = current_generation_num
+      JOptionPane.show_message_dialog(nil, "Generation ##{current_generation_num} is the last generation.")
     else
-      next_generation = life_model.next_generation_model
-
-      # Last generation is defined as the generation after which
-      # there are no changes.
-      if next_generation == life_model
-        @last_generation_num = current_generation_num
-        JOptionPane.show_message_dialog(nil, "Generation ##{current_generation_num} is the last generation.")
-        return
-      else
-        generations << next_generation
-      end
+      self.life_model = next_generation
+      self.current_generation_num = next_generation_num
     end
-
-    self.life_model = next_generation
-    self.current_generation_num = next_generation_num
-
   end
 
   def go_to_previous_generation
@@ -84,6 +69,63 @@ class LifeTableModel < AbstractTableModel
   end
 
   def add_current_num_change_handler(callable)
+    @generations.add_current_num_change_handler(callable)
+  end
+end
+
+
+class Generations < Array
+
+  attr_reader :life_model, :current_num, :last_num
+
+  def initialize(life_model)
+    @life_model = life_model
+    @current_num = 0
+    @last_num = nil
+    @current_num_change_handlers = []
+    ensure_next_in_cache
+  end
+
+  def current
+    self[current_num]
+  end
+
+  def current_num=(new_num)
+    need_to_notify_listeners = current_num != new_num
+    @current_num = new_num
+    if need_to_notify_listeners
+      @current_num_change_handlers.each { |handler| handler.call(@current_generation_num) }
+    end
+  end
+
+  def found_last_generation?
+    !!@last_num
+  end
+
+  def ensure_next_in_cache
+    if current_num == (size - 1) && (! found_last_generation?)
+      tentative_next = current.next_generation_model
+      if tentative_next == current
+        @last_num = current_num
+      else
+        self << tentative_next
+      end
+    end
+  end
+
+  def current_is_last?
+    last_num == current_num
+  end
+
+  def next
+    raise "Next was called when at end of lineage." if current_is_last?
+    @current_num += 1
+    ensure_next_in_cache
+    current
+  end
+
+  def add_current_num_change_handler(callable)
     @current_num_change_handlers << callable
   end
+
 end
