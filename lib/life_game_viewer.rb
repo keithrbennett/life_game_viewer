7cebb702f4bb379a9935d5c20be072a9419d6c90
Keith Bennett <keithrbennett@gmail.com>
2012-08-22 17:23:58 -0400

    Copied code from main.rb to life_game_viewer.rb. Added gemspec.

---
+++ lib/life_game_viewer.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+puts "Doing requires"
+
+require_relative 'model/life_model'
+require_relative 'view/life_table_model'
+require_relative 'view/main_frame'
+
+puts "Finished requires"
+
+class LifeGameViewer
+
+  def start_string
+    str = ''
+    20.times { str << ('*-' * 10) << "\n" }
+    str
+  end
+
+  def view
+
+    life_model = LifeModel.create_from_string(start_string)
+    puts "got life model, now starting viewer"
+
+    MainFrame.new(life_model).set_visible(true)
+  end
+
+  def self.hi
+    puts "Hello world!  Enjoy LifeGameViewer!"
+  end
+end
---
+++ life-game-viewer.gemspec
@@ -0,0 +1,12 @@
+
+Gem::Specification.new do |s|
+  s.name        = 'life-game-viewer'
+  s.version     = '0.0.0'
+  s.date        = Time.new
+  s.summary     = "Game of Life Viewer"
+  s.description = "Java Swing Game of Life Viewer written in JRuby"
+  s.authors     = ["Keith R. Bennett"]
+  s.email       = 'keithrbennett@gmail.com'
+  s.files       = Dir["lib/**/*.rb"]
+  s.homepage    = 'http://rubygems.org/gems/life-game-viewer'
+end
