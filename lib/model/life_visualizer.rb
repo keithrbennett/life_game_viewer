6f8eb10a9fff3e1f6525b0f3e92f4c80f43f7338
Keith R. Bennett <keithrbennett@gmail.com>
2012-08-26 19:00:38 -0400

    Moved copy_to_clipboard out of LifeVisualizer and into ClipboardHelper (self.clipboard_text=), w/mods. Added lostOwnership method, needed by Java for clipboard owners.

--- lib/model/life_visualizer.rb
+++ lib/model/life_visualizer.rb
@@ -1,9 +1,6 @@
 # Manages the string display of a life model.
 require_relative 'life_model'
 
-java_import 'java.awt.Toolkit'
-java_import 'java.awt.datatransfer.StringSelection'
-
 
 class LifeVisualizer
 
@@ -21,12 +18,4 @@ class LifeVisualizer
     
     output
   end
-
-  def copy_to_clipboard(model)
-    clipboard = Toolkit.default_toolkit.system_clipboard
-    string_selection = StringSelection.new(to_display_string(model))
-    clipboard.setContents(string_selection, self)
-  end
-
-
 end
