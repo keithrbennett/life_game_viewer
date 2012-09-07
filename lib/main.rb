61caf9ada967637898ae83dbd012dd9867d0c4f0
Keith Bennett <keithrbennett@gmail.com>
2012-09-07 06:41:53 -0400

    Added test to ensure that the program is run in JRuby. If not, it prints a descriptive message."

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+unless /java$/ === RUBY_PLATFORM
+  puts "This program must be run in JRuby."
+  exit -1
+end
+
 require_relative 'life_game_viewer'
 
 LifeGameViewer.new.view
